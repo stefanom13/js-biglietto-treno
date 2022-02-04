b5080237e9775d0b932312f5747fdf5cc1fdf50a
stefanom13 <stefanomancuso13@gmail.com>
2022-02-04 17:16:07 +0100

    inserimento prezzi e sconto

--- app.js
+++ app.js
@@ -10,16 +10,30 @@ console.log(userAge)
 const percorrenza =parseInt( prompt ('Quanti km vuoi percorrere?'))
 console.log(percorrenza)
 
+// calcolo prezzo biglietto
+
+const priceTrip = percorrenza * 0.21;
+console.log(priceTrip);
 
 
-// prima operazione 
+
+// adattiamo prezzo biglietto in base ad alcune richieste 
+      // 20% sconto se minorenni
+if (userAge <= 18 ) {
+const priceUndereighteen = priceTrip - ( ( 20 * priceTrip ) / 100 ); 
+console.log(priceUndereighteen);
+
+        // 40% sconto se over 65
+} else if ( userAge >= 65 ) {
+  const priceOversixteen = priceTrip - ( ( 40 * priceTrip ) / 100 );
+  console.log(priceOversixteen);
+
+}
 
 
-
-// età passeggero
-// calcolo prezzo biglietto in base a 
-    // 0.21 euro a kilomentro
-    // 20% sconto se minorenni
-    // 40% sconto se over 65
+                                     
+   
+ 
+  
 //  stampare prezzo finale con centesimi    
 
