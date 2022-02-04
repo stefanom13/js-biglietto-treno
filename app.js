4b28cf205fda9af543a1d1aba15f7727701d0b9c
stefanom13 <stefanomancuso13@gmail.com>
2022-02-04 16:41:55 +0100

    inserimento richiesta dati

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const userName =prompt('Ciao, il tuo nome è? ');
+console.log(userName)
+
+// età passeggero
+const userAge =parseInt( prompt ('Quanti hanni hai?'))
+console.log(userAge)
+
+// chiedere utente km vuole percorrere
+const percorrenza =parseInt( prompt ('Quanti km vuoi percorrere?'))
+console.log(percorrenza)
+
+
+
+// prima operazione 
+
+
+
+// età passeggero
+// calcolo prezzo biglietto in base a 
+    // 0.21 euro a kilomentro
+    // 20% sconto se minorenni
+    // 40% sconto se over 65
+//  stampare prezzo finale con centesimi    
+
